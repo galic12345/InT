b63d1284b6c2ad25281ac3a5ea3005a532c5a33d
Ivan <galic.ivan123321@gmail.com>
2024-09-11 12:14:04 +0200

    Spajanje odrađeno

--- app/src/main/java/com/example/anint/MainActivity.java
+++ app/src/main/java/com/example/anint/MainActivity.java
@@ -17,6 +17,12 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button add;
+
+    AlertDialog dialog;
+
+    LinearLayout layout;
+
 
 
     @Override
@@ -24,6 +30,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        add=findViewById(R.id.add);
+        layout=findViewById(R.id.container);
+
+        buildDialog();
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dialog.show();
+            }
+        });
+
+
+    }
+    public void buildDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog, null);
+
+        final EditText name=view.findViewById(R.id.nameEdit);
+
+        builder.setView(view);
+
+        builder.setView(view);
+        builder.setTitle("Dodaj zadatak")
+                .setPositiveButton("Spremi", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        addCard(name.getText().toString());
+
+                    }
+                })
+                .setNegativeButton("Prekid", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+
+                    }
+                });
+
+        dialog = builder.create();
+    }
+    private void addCard(String name){
+        final View view = getLayoutInflater().inflate(R.layout.card, null);
+
+        TextView nameView = view.findViewById(R.id.name);
 
     }
 }
